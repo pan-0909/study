6299930cc177abd9b6dd0d922628d718df696100
pan <148313358+pan-0909@users.noreply.github.com>
2024-02-21 16:33:17 +0800

    Update useCount.js

--- CompositionApi/demo/useCount.js
+++ CompositionApi/demo/useCount.js
@@ -1,1 +1,11 @@
+import {ref} from 'vue';
+function count (num) {
+  let count = ref(num);
+  let dounbleCount = computed(()=>count.value*2);
+  return {
+    count,
+    doubleCount
+  }
+}
 
+export default useCount;
