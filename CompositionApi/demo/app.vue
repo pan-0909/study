f61ccbfa3fd018e5bcae1d86ebc1ac2909b2fb7f
pan <148313358+pan-0909@users.noreply.github.com>
2024-02-21 16:29:38 +0800

    Update app.vue

--- CompositionApi/demo/app.vue
+++ CompositionApi/demo/app.vue
@@ -1,1 +1,9 @@
-
+<template>
+  <div>{{count}}</div>
+</temolate>
+<script setup>
+    // 在vue中引入
+    import useCount from './useCount.js'
+    let {count, doubleCount} = useCount(1);
+    console.log(count)
+</script>
